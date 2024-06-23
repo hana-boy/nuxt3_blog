7b417998c93736836d36b4c726396f7dd4dd9012
Kaihanafusa <85861384+Kaihanafusa@users.noreply.github.com>
2024-06-23 20:07:29 +0900

    create directories

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vue_blog
+# nuxt3_blog
+
+## バックエンド側
+https://github.com/hana-boy/golang_blog
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [
+    '@nuxt/devtools',
+  ],
+
+  css: ['/assets/css/style.css'],
+
+  app: {
+    head: {
+      title: 'Nuxt 3 basic',
+      meta: [{ name: 'description', content: 'Nuxt 3 for beginners' }],
+      link: [{ rel: 'icon', href: '/icon.png' }],
+    },
+  },
+
+  devtools: {
+    enabled: true,
+  },
+});
